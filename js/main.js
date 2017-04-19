5015201bde60df3446c0cf5e89f62fe65db151c4
Gerrett Rice <gerrettrice@gmail.com>
2017-04-19 15:37:09 -0400

    add favorite sticker after favorite menu items

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ $(document).ready(function () {
         speed: 1500
     });
 });
+
+//Add favorite sticker to selected menu items
+var favorite = $('.favorite');
+
+favorite.after('<img class="favoriteSticker" src="img/favorite.png">');
+
+var favoriteSticker = $('.favoriteSticker');
+
+favoriteSticker.css({
+    'width': '30px',
+    'display': 'inline'}
+);
