50bdb681989cae325acbfff40bf73455b0929166
Gerrett Rice <gerrettrice@gmail.com>
2017-04-19 15:36:14 -0400

    call text rotate plugin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-// Call SMINT jQuery Plugin
 $(document).ready(function () {
+
+    // Call SMINT jQuery Plugin
     $('.subMenu').smint({
         'scrollSpeed': 1000
+    });
+
+    //Call Text Rotate
+    $('.demo3 .rotate').textrotator({
+        animation: 'flipCube',
+        speed: 1500
     });
 });
